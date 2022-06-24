67c5761291d354f5beace0e9af12f54a3fa08b8f
Niki Hite <nikihite@comcast.net>
2022-06-24 12:22:32 -0700

    finished 5 tests

--- index.js
+++ index.js
@@ -3,26 +3,26 @@
 // OUTPUT: the value of the 'name' attribute i.e. Benny
 // REQS: use destructuring and the function should be a single line
 
-export const getName = () => {}
+export const getName = ({ name }) => name
 
 // INPUT: an object with a nested "address" attribute such as
 //   {name: 'Bob Smith', address: {street: 'Main Street', number: 123, city: 'Anytown', country: 'USA}}
 // OUTPUT: the string 'Bob Smith lives at 123 Main Street, Anytown, USA'
 // REQS: use destructuring and template literals
 
-export const printAddress = () => {}
+export const printAddress = ({ name, address: { street, number, city, country } }) => `${name} lives at ${number} ${street}, ${city}, ${country}`;
 
 // REFACTOR CHALLENGE
 // Refactor this function so that all values in the object are destructured
-// as part of the funciton definitions (i.e. there should be no dots in the template literals)
-export const printUserInfo = (user) => {
+// as part of the function definitions (i.e. there should be no dots in the template literals)
+export const printUserInfo = ({ username, name: { first, last }, info:{ favorites: { color, food }, pet: { name } , address: { number, street, city, country } }}) => {
   return `
-    Username: ${user.username},
-    Full Name: ${user.name.first} ${user.name.last},
-    Favorite Color: ${user.info.favorites.color},
-    Favorite Food: ${user.info.favorites.food},
-    Pet Name: ${user.info.pet.name},
-    Address: ${user.info.address.number} ${user.info.address.street}, ${user.info.address.city}, ${user.info.address.country}
+    Username: ${username},
+    Full Name: ${first} ${last},
+    Favorite Color: ${color},
+    Favorite Food: ${food},
+    Pet Name: ${name},
+    Address: ${number} ${street}, ${city}, ${country}
     `
 }
 
@@ -31,14 +31,23 @@ export const printUserInfo = (user) => {
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let acc = 0;
+  for (let num of rest) {
+    acc += num
+  }
+  return acc
+}
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {}
+export const getFirstTwoArgs = (...arr) => {
+  const [first, second, ...rest] = arr;
+  return ([first, second, [...rest]]);
+}
 
 // INPUT: an object with the following structure
 // {
